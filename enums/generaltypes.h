d28d3dd328362419ad9fd7eb37b0a3f678d37ef0
nagisc007 <nagisc007@yahoo.co.jp>
2022-06-27 15:00:22 +0900

    Add: general enum types.

---
+++ enums/generaltypes.h
@@ -0,0 +1,16 @@
+#ifndef GENERALTYPES_H
+#define GENERALTYPES_H
+
+enum class Category {
+    BookInfo,
+    Draft,
+    Plot,
+    Persons,
+    Worlds,
+    Research,
+    Notes,
+    Rubi,
+    Trash,
+};
+
+#endif // GENERALTYPES_H
