8c304329b0dbbfee2417944dd87b2a61d74b8eae
nagisc007 <nagisc007@yahoo.co.jp>
2022-07-02 15:42:10 +0900

    Fixed: key in draft tree.

--- views/drafttree.cpp
+++ views/drafttree.cpp
@@ -1,10 +1,11 @@
 #include "drafttree.h"
 
-#include "enums/generaltypes.h"
+#include "common/generalenums.h"
+#include "common/itemkeys.h"
 #include "items/treeitem.h"
 
 DraftTree::DraftTree(QWidget *parent)
-    : BaseTreeView{Category::Draft, parent}
+    : BaseTreeView{GeneralType::Category::Draft, parent}
 {
 
 }
@@ -12,14 +13,14 @@ DraftTree::DraftTree(QWidget *parent)
 // overrides
 TreeItem* DraftTree::CreateFileItem_()
 {
-    TreeItem *data = new TreeItem(ItemType::File, BaseCategoryOf());
-    data->SetData(0, "new file");
-    data->SetData(1, "synopsys");
-    data->SetData(2, "new text");
-    data->SetData(3, "note");
-    data->SetData(4, "pov");
-    data->SetData(5, "stage");
-    data->SetData(6, "date");
-    data->SetData(7, "time");
+    TreeItem *data = new TreeItem(GeneralType::ItemType::File, BaseCategoryOf());
+    data->SetData(ItemKeys::Draft::Title, "new file");
+    data->SetData(ItemKeys::Draft::Synopsys, "synopsys");
+    data->SetData(ItemKeys::Draft::Text, "new text");
+    data->SetData(ItemKeys::Draft::Note, "note");
+    data->SetData(ItemKeys::Draft::POV, "pov");
+    data->SetData(ItemKeys::Draft::Stage, "stage");
+    data->SetData(ItemKeys::Draft::Date, "date");
+    data->SetData(ItemKeys::Draft::Time, "time");
     return data;
 }
