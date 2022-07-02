6d84ef376330b465252503864eb351eac79ffb39
nagisc007 <nagisc007@yahoo.co.jp>
2022-07-02 13:59:53 +0900

    Add: general enums.

--- common/generalenums.h
+++ common/generalenums.h
@@ -1,5 +1,27 @@
 #ifndef GENERALENUMS_H
 #define GENERALENUMS_H
 
+namespace GeneralType {
+
+enum class Category {
+    BookInfo = 0,
+    Draft,
+    Plot,
+    Persons,
+    Worlds,
+    Research,
+    Notes,
+    Rubi,
+    Trash,
+    None = 99,
+};
+
+enum class ItemType {
+    Folder = 0,
+    File,
+    None = 99,
+};
+
+}
 
 #endif // GENERALENUMS_H
