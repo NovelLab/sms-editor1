871b9053126811e10f960f19bf84eba32f65ad92
nagisc007 <nagisc007@yahoo.co.jp>
2022-07-07 20:57:31 +0900

    Add: line number on text edit.

--- editor/textedit.cpp
+++ editor/textedit.cpp
@@ -5,14 +5,71 @@
 #include "utils/itemutility.h"
 
 #include <QTreeWidgetItem>
+#include <QPainter>
+#include <QTextBlock>
 
 TextEdit::TextEdit(QWidget *parent)
-    : QTextEdit{parent}
+    : QPlainTextEdit{parent}
 {
+    linenumber_area_ = new LineNumberArea(this);
 
+    connect(this, &TextEdit::blockCountChanged, this, &TextEdit::UpdateLineNumberAreaWidth);
+    connect(this, &TextEdit::updateRequest, this, &TextEdit::UpdateLineNumberArea);
+    connect(this, &TextEdit::cursorPositionChanged, this, &TextEdit::HighlightCurrentLine);
+
+    UpdateLineNumberAreaWidth(0);
+    HighlightCurrentLine();
+}
+
+// overrides (protected)
+void TextEdit::resizeEvent(QResizeEvent *event)
+{
+    QPlainTextEdit::resizeEvent(event);
+
+    QRect cr = contentsRect();
+    linenumber_area_->setGeometry(QRect(cr.left(), cr.top(), LineNumberAreaWidth(), cr.height()));
 }
 
 // methods
+void TextEdit::LineNumberAreaPaintEvent(QPaintEvent *event)
+{
+    QPainter painter(linenumber_area_);
+    painter.fillRect(event->rect(), Qt::lightGray);
+
+    QTextBlock block = firstVisibleBlock();
+    int blockNumber = block.blockNumber();
+    int top = qRound(blockBoundingGeometry(block).translated(contentOffset()).top());
+    int bottom = top + qRound(blockBoundingRect(block).height());
+
+    while (block.isValid() && top <= event->rect().bottom()) {
+        if (block.isVisible() && bottom >= event->rect().top()) {
+            QString number = QString::number(blockNumber + 1);
+            painter.setPen(Qt::black);
+            painter.drawText(0, top, linenumber_area_->width(), fontMetrics().height(),
+                             Qt::AlignRight, number);
+        }
+
+        block = block.next();
+        top = bottom;
+        bottom = top + qRound(blockBoundingRect(block).height());
+        ++blockNumber;
+    }
+}
+
+int TextEdit::LineNumberAreaWidth()
+{
+    int digits = 1;
+    int max = qMax(1, blockCount());
+    while (max >= 10) {
+        max /= 10;
+        ++digits;
+    }
+
+    int space = 3 + fontMetrics().horizontalAdvance(QLatin1Char('9')) * digits;
+
+    return space;
+}
+
 void TextEdit::SaveCurrentItem()
 {
     if (current_item_ && this->document()->isModified())
@@ -32,6 +89,43 @@ void TextEdit::UpdateView(const QTreeWidgetItem *item, bool isReadOnly)
         current_item_ = data;
         this->setReadOnly(isReadOnly);
     }
+}
+
+// slots (private)
+void TextEdit::UpdateLineNumberAreaWidth(int newBlockCount)
+{
+    Q_UNUSED(newBlockCount);
+    setViewportMargins(LineNumberAreaWidth(), 0, 0, 0);
+}
+
+void TextEdit::HighlightCurrentLine()
+{
+    QList<QTextEdit::ExtraSelection> extraSelections;
+
+    if (!isReadOnly()) {
+        QTextEdit::ExtraSelection selection;
+
+        QColor lineColor = QColor("#66666666").lighter(160);
+
+        selection.format.setBackground(lineColor);
+        selection.format.setProperty(QTextFormat::FullWidthSelection, true);
+        selection.cursor = textCursor();
+        selection.cursor.clearSelection();
+        extraSelections.append(selection);
+    }
+
+    setExtraSelections(extraSelections);
+}
+
+void TextEdit::UpdateLineNumberArea(const QRect &rect, int dy)
+{
+    if (dy)
+        linenumber_area_->scroll(0, dy);
+    else
+        linenumber_area_->update(0, rect.y(), linenumber_area_->width(), rect.height());
+
+    if (rect.contains(viewport()->rect()))
+        UpdateLineNumberAreaWidth(0);
 }
 
 // methods (private)
--- editor/textedit.h
+++ editor/textedit.h
@@ -1,25 +1,63 @@
 #ifndef TEXTEDIT_H
 #define TEXTEDIT_H
 
-#include <QTextEdit>
+#include <QPlainTextEdit>
 
+QT_BEGIN_NAMESPACE
 class QTreeWidgetItem;
+class QPaintEvent;
+class QResizeEvent;
+class QSize;
+class QWidget;
+QT_END_NAMESPACE
 
 class TreeItem;
 
-class TextEdit : public QTextEdit
+class TextEdit : public QPlainTextEdit
 {
     Q_OBJECT
 public:
     explicit TextEdit(QWidget *parent = nullptr);
 
+    void LineNumberAreaPaintEvent(QPaintEvent *event);
+    int LineNumberAreaWidth();
+
     void SaveCurrentItem();
     void UpdateView(const QTreeWidgetItem *item, bool isReadonly = false);
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
+private slots:
+    void UpdateLineNumberAreaWidth(int newBlockCount);
+    void HighlightCurrentLine();
+    void UpdateLineNumberArea(const QRect &rect, int dy);
 
 private:
     void SaveToCurrentItem_();
 
+    QWidget *linenumber_area_;
     TreeItem *current_item_ = nullptr;
 };
 
+class LineNumberArea : public QWidget
+{
+public:
+    LineNumberArea(TextEdit *editor) : QWidget(editor), editor_(editor)
+    {}
+
+    QSize sizeHint() const override
+    {
+        return QSize(editor_->LineNumberAreaWidth(), 0);
+    }
+
+protected:
+    void paintEvent(QPaintEvent *event) override
+    {
+        editor_->LineNumberAreaPaintEvent(event);
+    }
+
+private:
+    TextEdit *editor_;
+};
 #endif // TEXTEDIT_H
